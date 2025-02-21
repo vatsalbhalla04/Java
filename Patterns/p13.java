af9901780e69240c660568045606b425126b4fd2
Vatsal bhalla <118548880+vatsalbhalla04@users.noreply.github.com>
2025-02-21 18:09:39 +0530

    Pattern Programs

---
+++ Patterns/p13.java
@@ -0,0 +1,13 @@
+//Write program to print 0 if column is even and 1 if column is odd which is gradually decreasing  
+public class p13 {
+    public static void main(String[] args) {
+        int n  = 5 ; 
+        for(int i = 0 ; i<=n  ;i++){
+            for(int j=n ; j>=i ; j--){
+                if(j % 2 == 0 )System.out.print("0");
+                else System.out.print("1");
+            }
+            System.out.println();
+        }
+    }
+}
