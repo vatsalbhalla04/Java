90d14fc14981c86c3c7c056fd68862486d8adcf8
Vatsal bhalla <118548880+vatsalbhalla04@users.noreply.github.com>
2025-02-23 23:16:12 +0530

    Pattern

---
+++ Patterns/p20.java
@@ -0,0 +1,16 @@
+// Print the pyramid : 
+public class p20 {
+    public static void main(String[] args) {
+        int n = 5;
+        // Pyramid : 
+        for(int i = 0 ; i<=n ; i++){
+            for(int j = n  ; j>=i ; j--){
+                System.out.print("-");
+            }
+            // for(int j = 0 ; j<=i ; j++){
+            //     System.out.print("* ");
+            // }
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns/p21.java
@@ -0,0 +1,16 @@
+// Print the up-side down pyramid : 
+public class p21 {
+    public static void main(String[] args) {
+        int n = 5 ; 
+       for(int i = 0 ; i<n ; i++){
+        for(int j = 0 ; j<=i ; j++){
+            System.out.print("-"); // printing spaces : "-"
+        }
+        for(int j = n ; j>i ; j--)
+        {
+            System.out.print("* ");
+        }
+        System.out.println();
+       }
+    }
+}
