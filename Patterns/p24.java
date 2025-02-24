cca326f49da926803472ec7ecce4902b842e2976
Vatsal bhalla <118548880+vatsalbhalla04@users.noreply.github.com>
2025-02-24 18:27:35 +0530

    Pattern

---
+++ Patterns/p24.java
@@ -0,0 +1,28 @@
+// Sand-glass Pattern
+public class p24 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // Upside-Down Pyramid (Top Half)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Pyramid (Bottom Half)
+        for (int i = 0; i < n; i++) {
+            for (int j = n - i - 1; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns/p25.java
@@ -0,0 +1,40 @@
+// Hollow Sandglass Pattern
+public class p25 {
+    public static void main(String[] args) {
+        int n = 7; // Adjust size as needed
+        int i , j; 
+        for (i = 1; i <= n; i++) {
+            // inner loop to print spaces.
+            for (j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            // inner loop to print value of j.
+            for (j = i; j <= n; j++) {
+                if(j==i||j==n||i==1)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+
+            // printing new line for each row
+            System.out.println();
+        }
+
+        // Printing the lower part
+        for (i = n - 1; i >= 1; i--) {
+            // inner loop to print spaces.
+            for (j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            // inner loop to print value of j.
+            for (j = i; j <= n; j++) {
+                if(j==i||j==n||i==1)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            // printing new line for each row
+            System.out.println();
+        }
+    }
+}
