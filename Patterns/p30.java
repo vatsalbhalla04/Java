1b14263ce4d969908b4f3d5204cdea4a6a8bb0bc
Vatsal bhalla <118548880+vatsalbhalla04@users.noreply.github.com>
2025-02-27 10:23:27 +0530

    Pattern

---
+++ Patterns/p30.java
@@ -0,0 +1,33 @@
+// Butter fly star pattern : 
+public class p30 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                // Spaces - 2 * (n - i)
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // Outer loop
+        for (int i = n - 1; i >= 1; i--) {
+            // i = n - 1 --> do not want the middle row to be duplicated. 
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns/p31.java
@@ -0,0 +1,20 @@
+// pascal's triangle : 
+public class p31 {
+    public static void main(String[] args) {
+         int  n = 3 ;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= n - i; j++) {
+                // for left spacing
+                System.out.print(" ");
+            }
+            // used to represent x(i, k)
+            int x = 1;
+            for (int k = 1; k <= i; k++) {
+                // The first value in a line is always 1
+                System.out.print(x + " ");
+                x = x * (i - k) / k;
+            }
+            System.out.println();
+        }
+    }
+}
