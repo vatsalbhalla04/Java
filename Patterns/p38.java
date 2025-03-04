b43bbbdf6b1337325f43667777ab98daf8b2b729
Vatsal bhalla <118548880+vatsalbhalla04@users.noreply.github.com>
2025-03-04 17:36:47 +0530

    Pattern

---
+++ Patterns/p38.java
@@ -0,0 +1,29 @@
+public class p38 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 0; i < n; i++) {
+            // Printing leading spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            // Printing left side stars
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("* ");
+            }
+
+            // Printing spaces in the middle
+            for (int j = 0; j < 2 * i; j++) {
+                System.out.print(" ");
+            }
+
+            // Printing right side stars
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns/p39.java
@@ -0,0 +1,15 @@
+//Reverse Left Half Pyramid Pattern
+public class p39 {
+    public static void main(String[] args) {
+        int n = 5 ; 
+        for(int i = 0 ; i<=n ; i++){
+            for(int j = 0 ;j<=i ; j++){
+                System.out.print(" ");
+            }
+            for(int j = n ; j>=i ; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
