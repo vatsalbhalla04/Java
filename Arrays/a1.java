e051f6ddd73900dca7c079829725c0723f2a3208
Vatsal bhalla <118548880+vatsalbhalla04@users.noreply.github.com>
2025-03-11 14:09:12 +0530

    Arrays

---
+++ Arrays/a1.java
@@ -0,0 +1,9 @@
+// simple array question on array declaration
+public class a1{
+    public static void main(String[] args) {
+        int []arr = new int[10];
+
+        arr[0] = 5 ; 
+        System.out.println(arr[0]);
+    }
+}
---
+++ Arrays/a2.java
@@ -0,0 +1,10 @@
+// Length function Array
+public class a2 {
+    public static void main(String args[]) {
+        int arr[] = {10, 20, 30, 40, 50};
+        for(int i=0; i < arr.length; i++)
+        {
+              System.out.println(" " + arr[i]);              
+        }
+    }
+}
