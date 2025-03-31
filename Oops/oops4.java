8bcbc5ab859ed5d201520f6d006ca3a58e519620
Vatsal bhalla <118548880+vatsalbhalla04@users.noreply.github.com>
2025-03-31 21:46:48 +0530

    Oops

---
+++ Oops/oops4.java
@@ -0,0 +1,27 @@
+// Ref obj using "final" keyowrd : 
+class Car {
+    String model ; 
+
+    Car(String model){
+        this.model = model;
+    }
+    void display(){
+        System.out.println("Car model :" + model);
+    }
+}
+
+public class oops4 {
+    public static void main(String[] args) {
+        // Creating a new obj using the constructor : 
+        final Car car1 = new Car("Tata");
+        
+        // car1 = new Car("Tesla"); // creating a new ref is not allowed when final is already decalred 
+        car1.display();
+
+        // car2 is assigned to car1's ref : 
+        Car car2 = car1;
+        car2.model = "Mahindra";
+        car2.display();
+        car1.display();
+    }
+}
---
+++ Oops/oops5.java
@@ -0,0 +1,24 @@
+// Basic Math Program : 
+class math {
+    int a , b ; 
+    void max(){
+        if(a>b) System.out.println(a + " is max");
+        else System.out.println(b + " is max");
+    }
+    void add(){
+        System.out.println("Addition : " +(a+b));
+    }
+}
+
+public class oops5 {
+    public static void main(String[] args) {
+        math m1 = new math();
+
+        m1.a = 22;
+        m1.b = 23 ; 
+        System.out.println("A = " + m1.a + " B = " + m1.b); 
+
+        m1.max();
+        m1.add();
+    }
+}
